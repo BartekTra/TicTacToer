ac15be2df78c078180296ef81be445c89493908b
Bartek <bartosztra@gmail.com>
2025-02-23 00:44:21 +0100

    removed posts controller

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root "posts#index"
-  resources :posts
   
   post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
