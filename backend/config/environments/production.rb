dd88f1ee62f67d0fed0c2cff60af8f4fbd974b33
Bartek <bartosztra@gmail.com>
2025-02-23 21:10:45 +0100

    WebSocket config, maybe works(?)

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://react-rails-tictactoe.onrender.com/cable"
+  config.action_cable.allowed_request_origins = [ "https://react-rails-tictactoe.onrender.com", "https://react-rails-tictactoe-1.onrender.com"]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
