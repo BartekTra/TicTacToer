852ea96292819e3f9a7b8295e4dc4568deb467f2
bartek <bartosztra@gmail.com>
2026-04-14 17:14:07 +0200

    Update query_type queries

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -7,7 +7,7 @@ module Types
     field :fetch_gamestate, resolver: Queries::Game::FetchGamestate, description: "Fetch gamestate of game currently played by user based on tokens in cookies"
 
     if Rails.env.development?
-      field :fetch_all_games, resolver: Queries::Game::FetchAllGames, description: "Fetch every game going on in the app, test purposes only"
+      field :fetch_all_games, resolver: Queries::Game::FetchAllGames, description: "Fetch every game going on in the app, test purposes only and only in dev mode"
     end
   end
 end
